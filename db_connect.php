b8ea0d0735a066803e71e630232a76d2a6ecac57
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2025-09-02 19:11:15 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "db_dashboard";
+$host = "153.92.15.64"; // biasanya tetap 'localhost' di Hostinger, meskipun hosting-nya online
+$user = "u420934953_logitrack";
+$password = "L0g1_tr4ck!"; // masukkan password database Hostinger kamu
+$dbname = "u420934953_logitrack";
 
-$conn = new mysqli('localhost', 'root', '', 'db_dashboard', null, '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock');
+// Koneksi ke database Hostinger
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
