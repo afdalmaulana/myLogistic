1844e5cb26b2b5ec31f53f8c6e607e30f0c7a6fa
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2025-10-21 19:44:09 +0800

    Update submission-out.php

--- includes/submission-out.php
+++ includes/submission-out.php
@@ -693,7 +693,7 @@ while ($row = $result->fetch_assoc()) {
         <h4 style="font-weight: 800; font-size:28px;">Submission Overview</h4>
         <?php if ($isKanwil): ?>
             <div class="tabs">
-                <button class="tabslinks" onclick="openTab(event, 'forward')">Request <span class="badge"><?= $incompleteCount ?></span></button>
+                <button class="tabslinks" onclick="openTab(event, 'forward')">Request <span class="badge"><?= $forwardCount ?></span></button>
                 <button class="tabslinks" onclick="openTab(event, 'approved')">Complete <span class="badge"><?= $completeCount ?></span></button>
             </div>
         <?php else: ?>
